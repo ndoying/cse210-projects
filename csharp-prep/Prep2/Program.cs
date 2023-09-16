13ae29fa5c69379675d59b8b1715c2e5a308d8f2
ndoying <ndoying@snet.net>
2023-09-16 14:39:08 -0400

    Completed Grade program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (grade is > 70)
+        {
+            Console.WriteLine("Congratulations you passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you did not pass the course but you can re-enroll and try again.");
+        }
     }
 }
