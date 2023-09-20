b7d1e64150100650f73b363d9cbd8fd9f74d8b1f
ndoying <ndoying@snet.net>
2023-09-19 21:21:10 -0400

    Finished Prep 3 - syntax!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+    
+        // Console.Write("What is the Magic Number? ");
+        // int number = int.Parse(Console.ReadLine());
+        
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(0,101);
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+        
+        while (number != guess)
+        {
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+            }
+        }
+        
+        Console.WriteLine("You guessed it!");
+        
+
     }
 }
