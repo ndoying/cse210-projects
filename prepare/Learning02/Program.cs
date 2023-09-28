f6f51e4fbf12e5392711f6d3f4d77345ac4e19e9
ndoying <ndoying@snet.net>
2023-09-27 21:48:59 -0400

    Completed Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Create a new job instance
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        //Create a second new job instance
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        //Create a resume instance
+        Resume myResume = new Resume();
+        myResume._name = "Alison Rose";
+        
+        //Create a list of job objects
+        List<Job> _jobs = new List<Job>();
+
+        //Add job objects to list
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //Method to display resume details
+        myResume.DisplayResumeDetails();
+
+        
     }
 }
