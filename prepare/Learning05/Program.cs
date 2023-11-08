b832e75b25c73df58deea1015a0ffba0f45ec629
ndoying <ndoying@snet.net>
2023-11-07 22:16:31 -0500

    W09 Prepare - Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5.0));
+        shapes.Add(new Rectangle("Blue", 5.0, 7.0));
+        shapes.Add(new Circle("Yellow", 5.0));
+
+        foreach (Shape shape in shapes)
+        {
+            string _color = shape.GetColor();
+            double _area = shape.GetArea();
+
+            Console.WriteLine($"The {_color} shape has an area of: {_area}");
+        }
+        
     }
 }
